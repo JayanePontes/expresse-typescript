66e07a13b05cd5c6bb1b4422b9ffea134f299dda
Jayane Pontes <pontesjayane@gmail.com>
2022-07-28 15:47:20 -0300

    Adicionando middleware de erro

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import 'express-async-errors';
 
 const app = express();
 
@@ -10,6 +11,27 @@ const PORT = 8000;
 app.get('/', (req: Request, res: Response) => { 
     res.status(StatusCodes.OK).send('Express + typescript');
 });
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+  next();
+});
+
 
 app.listen(PORT, () => {
     console.log(`ounvindo a porta ${PORT}`);
